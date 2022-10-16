1190dfe3c5c2b863502bba78aebd55421feee826
Sai Rama Raju Vachavai <103111083+21MH5A0366@users.noreply.github.com>
2022-10-16 07:55:44 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     float r;     float p;     cin>>r;     cin>>p;     float multiplication;     multiplication=(r*p);     printf("%.2f",multiplication); }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.c
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    float r;
+    float p;
+    cin>>r;
+    cin>>p;
+    float multiplication;
+    multiplication=(r*p);
+    printf("%.2f",multiplication);
+}
+
