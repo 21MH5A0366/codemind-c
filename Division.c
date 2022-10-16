c82ff1688650e31a92472dc967dd1bec53782c1a
Sai Rama Raju Vachavai <103111083+21MH5A0366@users.noreply.github.com>
2022-10-16 07:51:08 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int r;     int p;     cin>>r;     cin>>p;     float division;     division=(r/p);     printf("%.0f",division); }

---
+++ Division.c
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int r;
+    int p;
+    cin>>r;
+    cin>>p;
+    float division;
+    division=(r/p);
+    printf("%.0f",division);
+}
