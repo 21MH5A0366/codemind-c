a7fbf9539bdc58853db9a767bf835b87625bb7d5
Sai Rama Raju Vachavai <103111083+21MH5A0366@users.noreply.github.com>
2022-10-16 07:54:14 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int r;     int p;     cin>>r;     cin>>p;     float multiplication;     multiplication=(r*p);     printf("%.0f",multiplication); }

---
+++ Multiplication.c
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int r;
+    int p;
+    cin>>r;
+    cin>>p;
+    float multiplication;
+    multiplication=(r*p);
+    printf("%.0f",multiplication);
+}
+
