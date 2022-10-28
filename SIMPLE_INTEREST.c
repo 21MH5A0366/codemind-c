ed3e442186f5f1c1bf4cff51519c9bfce293e825
Sai Rama Raju Vachavai <103111083+21MH5A0366@users.noreply.github.com>
2022-10-28 14:48:09 +0530

    #include<bits/stdc++.h> using namespace std; int main(){     int P;     int T;     int R;     cin>>P>>T>>R;     float interest;     interest=((P*R*T)/100);     printf("%.0f",interest); }

---
+++ SIMPLE_INTEREST.c
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int P;
+    int T;
+    int R;
+    cin>>P>>T>>R;
+    float interest;
+    interest=((P*R*T)/100);
+    printf("%.0f",interest);
+}
